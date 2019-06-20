c4f00a08da3d99bb204df0c3b7d07658e418aba9
Anthony-Ben <anthony.ben@mobileappscompany.us>
2019-06-20 18:16:18 -0400

    Init commit

---
+++ app/src/main/java/com/example/week1day4/Person.java
@@ -0,0 +1,56 @@
+package com.example.week1day4;
+
+public class Person {
+    private String firstName, lastName, streetAddress,
+            city, state, zip;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+
+}
